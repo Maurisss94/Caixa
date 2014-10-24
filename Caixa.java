a4953667e0313997db2bf774fcbb5a74a3d20ecd
Mauro <maurobale94@hotmail.es>
2014-10-24 12:25:15 +0200

    First commit

---
+++ Caixa.java
@@ -0,0 +1,53 @@
+package net.mauro;
+
+public class Caixa {
+    boolean oberta;
+    int amplada;
+    int altura;
+    String material;
+
+    //crear constructor
+    public Caixa(){
+        oberta = true;
+        material = "Cartró";
+
+    }
+    public Caixa(boolean estaoberta){
+        oberta = estaoberta;
+
+
+    }
+
+
+
+    public Caixa(boolean b, String string) {
+        // TODO Auto-generated constructor stub
+        oberta = b;
+        material = string;
+    }
+    public int getAmplada() {
+        return amplada;
+    }
+
+    public void setAmplada(int amplada) {
+        this.amplada = amplada;
+    }
+
+    public boolean isOberta(){
+        return  oberta;
+    }
+
+    public void obre(){
+        oberta = true;
+    }
+
+    public void tanca(){
+        oberta = false;
+    }
+    public boolean compara(Caixa nova){
+        return this.amplada == nova.getAmplada();
+    }
+
+
+
+}
